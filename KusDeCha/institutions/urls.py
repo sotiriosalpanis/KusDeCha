16007b4e5e88dd8f848df92dea6726182c6bec1b
salpanis <salpanis@gmail.com>
2021-04-14 16:11:30 +0100

    Get single inst API endpoint working

--- KusDeCha/institutions/urls.py
+++ KusDeCha/institutions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InstitutionListView
+from .views import InstitutionListView, InstitutionDetailView
 
 urlpatterns = [
-    path('',InstitutionListView.as_view())
+    path('',InstitutionListView.as_view()),
+    path('<int:pk>/',InstitutionDetailView.as_view())
 ]
--- KusDeCha/institutions/views.py
+++ KusDeCha/institutions/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Institution
 from .serializers.common import InstitutionSerializer
@@ -11,3 +12,16 @@ class InstitutionListView(APIView):
         institutions = Institution.objects.all()
         serialized_institutions = InstitutionSerializer(institutions, many=True)
         return Response(serialized_institutions.data, status=status.HTTP_200_OK)
+
+class InstitutionDetailView(APIView):
+
+    def get_institution(self, pk):
+        try:
+            return Institution.objects.get(pk=pk)
+        except Institution.DoesNotExist:
+            raise NotFound(detail="Cannot find the institution requested")
+
+    def get(self, _request, pk):
+        institution = self.get_institution(pk=pk)
+        serialized_institution = InstitutionSerializer(institution)
+        return Response(serialized_institution.data, status=status.HTTP_200_OK)
